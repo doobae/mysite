1e770025424a0249fcbdcc4f6ab225bdba428632
doobae <kimlab869@gmail.com>
2022-10-06 22:37:13 +0900

    no message

---
+++ config/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+from pybo import views
+from django.contrib import admin
+from django.urls import path
+from pybo import views
+urlpatterns = [
+path('admin/', admin.site.urls),
+path('pybo/', views.index),
+]
